27dace61ea6dc40bba6c091919ac8dca1e2da2bf
shuhrat2703 <jakhongirislomjonov@gmail.com>
2024-08-28 08:43:56 -0700

    three commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        double num1 = 10;
+/*        double num1 = 10;
         double num2 = 3;
         double res1 = num1 / num2;
         double res2 = num1 * num2;
@@ -13,6 +14,23 @@ public class Main {
         System.out.println(res3);
         System.out.println(res4);
         double res5 = num1 % num2;
-        System.out.println(res5);
+        System.out.println(res5);*/
+
+
+       /* byte smallestNumber;
+        Scanner input = new Scanner(System.in);
+        smallestNumber=input.nextByte();
+        System.out.println("input number ");*/
+
+        System.out.print("Enter radius :");
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+
+
+
+        double area =  ( Math.PI * Math.pow(radius, 2));
+        System.out.println(" Display the area : " + area);
+
+
     }
 }
