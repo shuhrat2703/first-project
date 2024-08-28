8853bb42288cd50171207ac2efbca6c82ad7b4c3
shuhrat2703 <jakhongirislomjonov@gmail.com>
2024-08-28 07:58:28 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        double num1 = 10;
+        double num2 = 3;
+        double res1 = num1 / num2;
+        double res2 = num1 * num2;
+        double res3 = num1 - num2;
+        double res4 = num1 + num2;
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);
+        double res5 = num1 % num2;
+        System.out.println(res5);
     }
 }
